e0ac57ed7ddac6487066e046bb5efa76fe276d75
AliciaTak <tak.alicia@gmail.com>
2025-12-26 17:14:41 +1100

    Temporary: empty branch placeholder until content is fixed

--- qme_dev_hourly/.cfg_149870987.gadi-pbs/set_env_var.sh
+++
@@ -1,2 +0,0 @@
-export DASK_PBS_SCHEDULER="/g/data/xv83/users/at2708/bias_adjustment_acs_qme/qme_dev_copy_hourly/.cfg_149870987.gadi-pbs/scheduler.json"
-export SINGULARITYENV_DASK_PBS_SCHEDULER="/g/data/xv83/users/at2708/bias_adjustment_acs_qme/qme_dev_copy_hourly/.cfg_149870987.gadi-pbs/scheduler.json"
--- qme_dev_hourly/.cfg_150021512.gadi-pbs/set_env_var.sh
+++
@@ -1,2 +0,0 @@
-export DASK_PBS_SCHEDULER="/g/data/xv83/users/at2708/bias_adjustment_acs_qme/qme_dev_copy_hourly/.cfg_150021512.gadi-pbs/scheduler.json"
-export SINGULARITYENV_DASK_PBS_SCHEDULER="/g/data/xv83/users/at2708/bias_adjustment_acs_qme/qme_dev_copy_hourly/.cfg_150021512.gadi-pbs/scheduler.json"
